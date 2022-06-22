cedae7e8f813319c1ffdadfd1d3e5be46d7a5318
EJamesssss <ejames.hegina@gmail.com>
2022-06-22 15:03:44 +0800

    add validation testing

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,6 @@ class Category < ApplicationRecord
     belongs_to :user
     has_many :tasks, dependent: :destroy
 
-    validates :category_title, presence: true, uniqueness: { scope: :user_id, case_sensitive: false, message: "already exist"}
+    validates :category_title, presence: true, uniqueness: { scope: :user_id, message: "already exist"}
     validates :category_details, presence: true
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "shoud not save if existing name on same user" do
+    category = Category.new(category_title: "Test", user_id:1)
+    assert category.save
+    category = Category.new(category_title: "Test", user_id:1)
+    assert_not category.save
+  end
+
+  
 end
